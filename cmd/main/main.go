cb5c865a8f854dac4a3599fb0e120c98a7f0f2c9
Armaan Mishra <armaanmishra48@gmail.com>
2024-09-07 02:23:34 +0530

    setup server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/armaanx/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
